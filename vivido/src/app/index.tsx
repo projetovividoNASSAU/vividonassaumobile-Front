80307e48c5e52f778828d337c42401580b3197de
BrunaCardoso7 <obitadrawing@gmail.com>
2024-09-07 21:54:18 -0300

    feat: add nativewind and expo-router

---
+++ vivido/src/app/index.tsx
@@ -0,0 +1,12 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+    return (
+        <View className="flex-1 justify-center items-center">
+            <StatusBar style="auto" />
+            <Text>Open up App.tsx to start working on your app!</Text>
+        </View>
+    );
+}
+
