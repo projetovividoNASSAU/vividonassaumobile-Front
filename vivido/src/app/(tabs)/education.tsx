9ae5c291a3b9976994774a9cc5e1718d14d359d1
BrunaCardoso7 <obitadrawing@gmail.com>
2024-10-01 00:46:24 -0300

    feat: add article screen and mock data

--- vivido/src/app/(tabs)/education.tsx
+++ vivido/src/app/(tabs)/education.tsx
@@ -1,18 +1,30 @@
-//(tabs)/index.tsx
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, FlatList } from 'react-native';
+import { Article } from '../../components/Article';
+import Header from '../../components/Header';
+import { articleData } from '../../mocks/mock-articles-data';
 
 export default function EducationTab() {
   return (
-    <View style={styles.container}>
-      <Text>Em constução...</Text>
-    </View>
+    <>
+      <Header 
+        titleHeader='Artigos'
+        subtitleHeader='Leia mais sobre o assunto'
+      />
+      <View className='flex-1 w-full justify-center items-center bg-white'>
+        <FlatList 
+          contentContainerStyle={{width: '100%', display: "flex", justifyContent:"center", alignItems: "center"}}
+          data={articleData}
+          renderItem={({item}) => (
+            <Article
+              title={item.title}
+              subtitle={item.subtitle}
+              imageUrl={item.imageUrl}
+            />
+          )}
+        />
+        {/* <Article/> */}
+      </View>
+    </>
   );
 }
 
-const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-    },
-})
---
+++ vivido/src/mocks/mock-articles-data.ts
@@ -0,0 +1,32 @@
+export const articleData = [
+    {
+        id: 1,
+        title: "A Escalada da Violência Urbana",
+        subtitle: "Entenda as causas e impactos da violência nas grandes cidades.",
+        imageUrl: "images (1).jpeg"
+    },
+    {
+        id: 2,
+        title: "Violência Doméstica: Quebrando o Ciclo",
+        subtitle: "Como a sociedade pode ajudar vítimas de violência doméstica a romperem o silêncio.",
+        imageUrl: "download.jpeg"
+    },
+    {
+        id: 3,
+        title: "O Impacto da Violência na Juventude",
+        subtitle: "Uma análise sobre como a exposição à violência afeta o desenvolvimento de jovens.",
+        imageUrl: "images.jpeg"
+    },
+    {
+        id: 4,
+        title: "A Violência de Gênero no Século XXI",
+        subtitle: "Explorando os desafios persistentes da violência contra mulheres e minorias.",
+        imageUrl: "images.png"
+    },
+    {
+        id: 5,
+        title: "Violência nas Escolas: Um Problema Silencioso",
+        subtitle: "Como o bullying e a violência nas escolas afetam o ambiente educacional.",
+        imageUrl: "images1.jpeg"
+    },
+];
